c0761c1cc44e2177b0943a4eef12b9a2f75488ce
Robert Perez <rickymcmuffins@gmail.com>
2024-10-18 13:13:09 -0400

    finished vulkan tutorial!

---
+++ build-windows/CMakeFiles/GameTime.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GameTime.dir/src/main.cpp.obj"
+  "CMakeFiles/GameTime.dir/src/main.cpp.obj.d"
+  "GameTime.exe"
+  "GameTime.exe.manifest"
+  "GameTime.pdb"
+  "libGameTime.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GameTime.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ create-build-windows.sh
@@ -0,0 +1,8 @@
+mkdir -p build-windows
+
+cd build-windows
+
+cmake -G "Unix Makefiles" \
+      -DCMAKE_SYSTEM_NAME=Windows \
+      -D CMAKE_C_COMPILER=x86_64-w64-mingw32-gcc \
+      -D CMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ ..
